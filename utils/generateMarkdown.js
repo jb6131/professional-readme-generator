735c369efa3ed5d1cc8f03cb123e19fd56e7ddab
jb6131 <44068903+jb6131@users.noreply.github.com>
2023-08-13 16:35:15 -0500

    finished majority of generation

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,53 +13,57 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-  license badge here
+  ![GitHub License]
+
+  ## Description
+
+  ${data.description}
 
   ## Table of Contents
 
-  * Installation
+  * [Installation](#installation)
 
-  * Usage
+  * [Usage](#usage)
 
-  * License
+  * [License](#license)
 
-  * Contributing
+  * [Contributing](#contributing)
 
-  * Tests
+  * [Tests](#tests)
 
-  * Questions
+  * [Questions](#questions)
   
   ## Installation
 
   To install necessary dependencies, run the following command:
 
   \`\`\`bash
-  ${dependencies}
+  ${data.dependencies}
   \`\`\`
 
   ## Usage
 
-  ${usage}
+  ${data.usage}
 
   ## License
 
-  This project is licensed under the ${license} license.
+  This project is licensed under the ${data.license} license.
 
   ## Contributing
 
-  ${contributing}
+  ${data.contributing}
 
   ## Tests
 
   To run tests, run the following command:
 
   \`\`\`bash
-  ${tests}
+  ${data.tests}
   \`\`\`
 
   ## Questions
 
-  If you have any questions about the repo, open an issue or contact me directly at ${email}. You can find more of my work at ${username}.
+  If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at [${data.username}](https://github.com/${data.username}/).
 `;
 }
 
